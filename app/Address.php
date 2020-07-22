c34367849b58862d5bbe7b9e600f7a7c290a6c3d
Don-Tair93 <don.tair101@gmail.com>
2020-07-22 03:19:40 +0200

    translations

--- app/Address.php
+++ app/Address.php
@@ -5,11 +5,15 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 use TCG\Voyager\Traits\Spatial;
+use TCG\Voyager\Traits\Translatable;
 
 class Address extends Model
 {
-
+    use Translatable;
     use Spatial;
+
+
+    protected $translatable = ['address'];
     protected $spatial = ['location'];
 
 }
--- app/Service.php
+++ app/Service.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Translatable;
 
 
 class Service extends Model
 {
-    
+    use Translatable;
+    protected $translatable = ['name'];
+
 }
